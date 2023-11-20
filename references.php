b6518802d9b3bb283e057cbb3ce337e926df7a50
zakirah maisarah <129544321+aerasarah@users.noreply.github.com>
2023-11-20 14:13:55 +0800

    Create references.php

---
+++ references.php
@@ -0,0 +1,378 @@
+<html>
+<head>
+	<title>Payment Reference</title>
+
+	<style>
+		body
+		{
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+		}
+
+		.container
+        {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+        }
+
+		.header
+		{
+			background-color: beige;
+			color: black;
+			padding: 0px 0;
+			margin-top: 20px;
+			align-items: center;
+			margin-bottom: 10px;
+		}
+
+		.header-text
+		{
+			font-size: 26px;
+			justify-content: center;
+			align-items: center;
+			display: flex;
+		}
+
+		.content
+		{
+			background-color: beige;
+            text-align: center;
+            align-items: center;
+            margin-top: 10px;
+            width: 100%;
+            max-width: 1800px;
+            padding: 0px 0;
+		}
+
+		.content-text
+		{
+			font-size: 20px;
+		}
+
+		.title
+		{
+			background-color: beige;
+            text-align: center;
+            align-items: center;
+            margin-top: 100px;
+            padding: 0px 0;
+		}
+
+		.title-text
+		{
+			font-size: 26px;
+			justify-content: center;
+			align-items: center;
+			display: flex;
+		}
+
+		table {
+            border-collapse: collapse;
+            margin-top: 40px;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+
+        .table, th, td {
+            border: 1px solid black;
+            padding: 10px 40px;
+            text-align: center;
+            font-size: 20px;
+            align-items: center;
+        }
+        
+        .td:first-child {
+            width: 10px; /* Set the width of the first column */
+        }
+
+        .center-table 
+        {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+        }
+
+        .back-button {
+            background-color: indianred;
+            color: white;
+            font-size: 20px;
+            padding: 13px 30px;
+            border: none;
+            cursor: pointer;
+            transition: background-color 1.0s; /* Add a smooth color transition on hover */
+            margin-right: 10px; /* Add margin to separate the buttons */
+            margin-top: 50px;
+        }
+
+		.back-button:hover 
+		{
+			background-color: lightgray; /
+		}
+
+	</style>
+</head>
+<body>
+	<body style="background-color: beige;"></body>
+
+	<form action="home.php" method="get">
+		<div class="container">
+			<div class="header">
+				<div class="header-text">
+					<br><strong>BAYARAN YURAN PENDAFTARAN / PERKHIDMATAN BAGI SEM 2 DAN KE ATAS</div>
+				<div class="header-text" style="color: mediumvioletred; margin-top: -10px;"><br>SERVICE FEE PAYMENT FOR SEM 2 AND ABOVE</strong></div>
+			</div>
+			<div class="content">
+				<div class="content-text">
+					<br>Para pelajar Kolej / Universiti Poly-Tech MARA Semester 2 dan ke atas <strong>diwajibkan</strong> membuat bayaran <strong>yuran pendaftaran / perkhidmatan</strong> untuk sesi JULAI 2023 melalui sistem FPX.
+				</div>
+				<div class="content-text" style="color: mediumvioletred; margin-top: -10px">
+				<br> Poly-Tech MARA College/University students from Semester 2 and above, it is <b>mandatory</b> to make <b>registration / service fee payments</b> for the July 2023 session through the FPX system."
+				</div>
+			</div>
+			<div class="content">
+				<div class="content-text">
+					<br>Pembayaran mesti dijelaskan sebagaimana yang dinyatakan di dalam <strong>jadual pembayaran</strong> berikut mengikut <strong>KPTM</strong> dan <strong>UPTM</strong>
+				</div>
+				<div class="content-text" style="color: mediumvioletred; margin-top: -10px">
+				<br>Payment must be made as stated in the following <strong>payment schedule</strong> based on <strong>KPTM</strong> and <strong>UPTM</strong> 
+				</div>
+			</div>		
+			<div class="title">
+				<div class="header-text">
+					<br><strong>- UPTM -</div>
+			</div>
+			<table style="margin-left: -550px; margin-top: 50px;">
+				<tr>
+					<td colspan="2"><strong>DIPLOMA</strong></td>
+				</tr>
+				<tr>
+					<th>SEMESTER</th>
+        			<th>YURAN PERKHIDMATAN<p style="color: mediumvioletred; margin-top: 7px;">SERVICE FEE (RM)</th>
+    			</tr>
+			    <tr>
+			        <td>2</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>3</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>4</td>
+			        <td>310.00</td>
+			    </tr>
+			    <tr>
+			        <td>5</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>6</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>7 (LI)</td>
+			        <td>50.00</td>
+			    </tr>
+			    <tr>
+			        <td>8 & 9</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>9</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>10</td>
+			        <td>310.00</td>
+			    </tr>
+			</table>
+		</div>
+			<div class="center-table">
+			<table style="margin-left: 600px; margin-top: -525px;">
+				<tr>
+					<td colspan="2"><strong>IJAZAH</strong></td>
+				</tr>
+				<tr>
+					<th>SEMESTER</th>
+        			<th>YURAN PERKHIDMATAN<p style="color: mediumvioletred; margin-top: 7px;">SERVICE FEE (RM)</th>
+    			</tr>
+			    <tr>
+			        <td>2</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>3</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>4</td>
+			        <td>310.00</td>
+			    </tr>
+			    <tr>
+			        <td>5</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>6</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>7</td>
+			        <td>310.00</td>
+			    </tr>
+			    <tr>
+			        <td>8</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>8 (LI)</td>
+			        <td>-</td>
+			    </tr>
+			    <tr>
+			        <td>9</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>9 (LI)</td>
+			        <td>-</td>
+			    </tr>
+			    <tr>
+			        <td>10</td>
+			        <td>310.00</td>
+			    </tr>
+			    <tr>
+			        <td>11</td>
+			        <td>260.00</td>
+			    </tr>
+			    <tr>
+			        <td>12</td>
+			        <td>260.00</td>
+			    </tr>
+			</table>
+		</div>
+			<table style="margin-left: 397px; margin-top: 70px;">
+				<tr>
+					<td colspan="2"><strong>PHD</strong></td>
+				</tr>
+				<tr>
+					<th>SEMESTER</th>
+        			<th>YURAN PERKHIDMATAN<p style="color: mediumvioletred; margin-top: 7px;">SERVICE FEE (RM)</th>
+    			</tr>
+			    <tr>
+			        <td>4</td>
+			        <td>300.00</td>
+			    </tr>
+			    <tr>
+			        <td>7</td>
+			        <td>300.00</td>
+			    </tr>
+			</table>
+			</div>
+			<table style="margin-left: 990px; margin-top: -218px;">
+				<tr>
+					<td colspan="2"><strong>PROFESSIONAL</strong></td>
+				</tr>
+				<tr>
+					<th>SEMESTER</th>
+        			<th>YURAN PERKHIDMATAN<p style="color: mediumvioletred; margin-top: 7px;">SERVICE FEE (RM)</th>
+    			</tr>
+			    <tr>
+			        <td>2</td>
+			        <td>280.00</td>
+			    </tr>
+			    <tr>
+			        <td>3</td>
+			        <td>330.00</td>
+			    </tr>
+			    <tr>
+			        <td>4</td>
+			        <td>280.00</td>
+			    </tr>
+			    <tr>
+			        <td>5</td>
+			        <td>330.00</td>
+			    </tr>
+			    <tr>
+			        <td>6</td>
+			        <td>280.00</td>
+			    </tr>
+			    <tr>
+			        <td>7,9,11</td>
+			        <td>330.00</td>
+			    </tr>
+			    <tr>
+			        <td>8,10,12</td>
+			        <td>280.00</td>
+			    </tr>
+			</table>
+		</div>
+			<div class="title">
+				<div class="header-text">
+					<br><strong>- YURAN ASRAMA -</div>
+					<div class="header-text" style="color: mediumvioletred; margin-top: 10px">
+					<br><strong>HOSTEL FEE</div>
+			</div>
+			<div class="center-table">
+			<table>
+			<tr>
+					<th>CAWANGAN<p style="color: mediumvioletred; margin-top: 7px;">BRANCH</th>
+        			<th>BAYARAN ASRAMA (CAWANGAN)<p style="color: mediumvioletred; margin-top: 7px;">HOSTEL FEE (BRANCH)</th>
+        			<th>JUMLAH KESELURUHAN UNTUK 4 BULAN<p style="color: mediumvioletred; margin-top: 7px;">TOTAL AMOUNT FOR 4 MONTHS</th>
+    			</tr>
+			    <tr>
+			        <td>UPTM</td>
+			        <td>RM200 sebulan / per month</td>
+			        <td>800.00</td>
+			    </tr>
+			</table>
+		</div>
+
+			<div class="title">
+				<div class="header-text">
+					<br><strong>- BAYARAN COMPOUND -</div>
+					<div class="header-text" style="color: mediumvioletred; margin-top: 10px">
+					<br><strong>COMPOUND FEE</div>
+			</div>
+			<div class="center-table">
+			<table style="width: 700px;">
+				<tr>
+					<th width="400px">KESALAHAN</th>
+        			<th width="100px">(RM)</th>
+    			</tr>
+			    <tr>
+			        <td><b>RAMBUT</b><p style="margin-top: 10px;">(berwarna, fesyen yang keterlaluan)</th>
+			        <td>10.00</td>
+			    </tr>
+			    <tr>
+			        <td><b>BAJU</b><p style="margin-top: 10px;">(pendek, lusuh)</td>
+			        <td>10.00</td>
+			    </tr>
+			    <tr>
+			        <td><b>T-SHIRT</b><p style="margin-top: 10px;">(tanpa kolar)</td>
+			        <td>10.00</td>
+			    </tr>
+			    <tr>
+			        <td><b>SELUAR</b><p style="margin-top: 10px;">(pendek, koyak, lusuh)</td>
+			        <td>10.00</td>
+			    </tr>
+			    <tr>
+			        <td><b>SELIPAR / CAPAL</b></td>
+			        <td>10.00</td>
+			    </tr>
+
+			</table>
+		</div>
+
+		<div class="center-table" style="margin-bottom: 40px;">
+			<div class="button">
+				<br><br><input type="submit" value="Back" class="back-button">
+			</div>
+		</div>
+		</div>
+	</form>
+</body>
+</html
